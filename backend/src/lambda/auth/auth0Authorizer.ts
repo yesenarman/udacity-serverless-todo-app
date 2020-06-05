fda0dc9a1e29a4c06956d245cb726715721392b7
Arman Yessenamanov <yesenarman@gmail.com>
2020-06-05 23:34:23 +0600

    feat: Implement Auth0 authorization token verification

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,25 +1,22 @@
-import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
-
-import { verify, decode } from 'jsonwebtoken'
-import { createLogger } from '../../utils/logger'
+import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import Axios from 'axios'
+import { verify, decode } from 'jsonwebtoken'
 import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
+import { createLogger } from '../../utils/logger'
 
 const logger = createLogger('auth')
 
-// TODO: Provide a URL that can be used to download a certificate that can be used
-// to verify JWT token signature.
-// To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+const jwksUrl = process.env.JWKS_URL || ''
+let signingKeys: { kid: string; publicKey: string }[]
 
 export const handler = async (
   event: CustomAuthorizerEvent
 ): Promise<CustomAuthorizerResult> => {
   logger.info('Authorizing a user', event.authorizationToken)
   try {
-    const jwtToken = await verifyToken(event.authorizationToken)
+    const jwtToken = await verifyToken(event.authorizationToken || '')
     logger.info('User was authorized', jwtToken)
 
     return {
@@ -57,18 +54,56 @@ export const handler = async (
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
   const jwt: Jwt = decode(token, { complete: true }) as Jwt
+  const secret: string = await getSecret(jwt.header.kid || '')
+  verify(token, secret, { algorithms: ['RS256'] })
+  return jwt.payload
+}
 
-  // TODO: Implement token verification
-  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
-  // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+async function getSecret(kid: string): Promise<string> {
+  const signingKey = (await getSigningKeys()).find((key) => key.kid === kid)
+  if (!signingKey) {
+    throw new Error('Signing key not found')
+  }
+  return signingKey.publicKey
+}
+
+async function getSigningKeys() {
+  if (!signingKeys) {
+    const keys = await Axios.get(jwksUrl).then((res) => res.data.keys || [])
+
+    signingKeys = keys
+      .filter(
+        (key: any) =>
+          key.use === 'sig' && // JWK property `use` determines the JWK is for signing
+          key.kty === 'RSA' && // We are only supporting RSA (RS256)
+          key.kid && // The `kid` must be present to be useful for later
+          ((key.x5c && key.x5c.length) || (key.n && key.e)) // Has useful public keys
+      )
+      .map((key: any) => {
+        return { kid: key.kid, publicKey: certToPEM(key.x5c[0]) }
+      })
+  }
+
+  return signingKeys
+}
+
+function certToPEM(cert: string) {
+  const match = cert.match(/.{1,64}/g)
+  if (match) {
+    cert = match.join('\n')
+    cert = `-----BEGIN CERTIFICATE-----\n${cert}\n-----END CERTIFICATE-----\n`
+  }
+  return cert
 }
 
 function getToken(authHeader: string): string {
-  if (!authHeader) throw new Error('No authentication header')
+  if (!authHeader) {
+    throw new Error('No authentication header')
+  }
 
-  if (!authHeader.toLowerCase().startsWith('bearer '))
+  if (!authHeader.toLowerCase().startsWith('bearer ')) {
     throw new Error('Invalid authentication header')
+  }
 
   const split = authHeader.split(' ')
   const token = split[1]
--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,8 @@
-// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
+const apiId = 's8qbsrt52c'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
-  // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-rjulv14u.auth0.com',             // Auth0 domain
+  clientId: 'YxSK4v41XC2r913yPhiLC1Wa1HTH1RTf', // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
